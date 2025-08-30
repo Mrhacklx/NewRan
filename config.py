54d5d91608941626d0c89017f10dcd8eebe183a2
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-08-30 17:01:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ API_HASH = environ.get('API_HASH', 'c428034cb811de290315501d8e3c82b5')
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://i.ibb.co/0R5MHnpt/what-are-bots-image1-710x400px-300dpi.jpg')).split() # Bot Start Picture
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6739173851').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8390398235').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "NewRan_bot") # without @
 PORT = environ.get("PORT", "8080")
 
