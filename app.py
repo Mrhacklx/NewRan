988d98c24ea67b2b1e39f1dafcfec96703ced25b
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-08-30 18:44:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
-from plugins.dbusers import db
 import os
 
 app = Flask(__name__)
@@ -9,14 +8,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'TechVJ'
-
-
-@app.route('/files')
-def list_files():
-    """Fetch all file_ids and show them as Telegram links."""
-    docs = await db.get_all_file_ids()
-    links = [f"https://t.me/NewRan_bot/start={doc['file_id']}" for doc in docs if "file_id" in doc]
-    return render_template("files.html", links=links)
 
 
 # ---------------- Run Server ----------------
